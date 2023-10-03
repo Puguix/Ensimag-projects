ff91340cf95226e313c756c0f53602f2629e521d
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2023-10-03 15:06:56 +0200

    end tp1

--- TP/MonteCarlo/tp1.cpp
+++ TP/MonteCarlo/tp1.cpp
@@ -19,7 +19,7 @@ PnlVect *getBrownianMotion(double N, double T, PnlRng *rng)
 
     // Compute the motion: Xn+1 = Xn + root * Gn+1
     pnl_vect_set(res, 0, 0);
-    for (int i = 2; i <= N; i++)
+    for (int i = 1; i <= N; i++)
     {
         pnl_vect_set(res, i, GET(res, i - 1) + root * GET(res, i));
     }
@@ -48,16 +48,12 @@ double monteCarloMean(int nbSamples, double a, double T, double N)
         {
             if (a > 0 && GET(bm, j) >= a)
             {
-                // res += T * (j - 1) / N + (T / N) * (a - GET(bm, j - 1)) / (GET(bm, j) - GET(bm, j - 1));
                 res += T * j / N;
-                // std::cout << T * j / N << " sup\n";
                 break;
             }
             if (a < 0 && GET(bm, j) <= a)
             {
-                // res += T * (j - 1) / N + (T / N) * (a - GET(bm, j - 1)) / (GET(bm, j) - GET(bm, j - 1));
                 res += T * j / N;
-                // std::cout << T * j / N << " inf\n";
                 break;
             }
             j++;
@@ -68,7 +64,70 @@ double monteCarloMean(int nbSamples, double a, double T, double N)
         }
 
         pnl_vect_free(&bm);
-        // std::cout << res << "\n";
+    }
+
+    return res / nbSamples;
+}
+
+double getPreciseTA(double a, double x1, double x2, double step, int nbStep, PnlRng *rng)
+{
+    double UminusS = 0;
+    double TminusU = 0;
+
+    int i = 1;
+    while (x1 < a && i <= nbStep)
+    {
+        double UminusS = i * step / nbStep;
+        double TminusU = step - UminusS;
+
+        x1 = TminusU / step * x1 + UminusS / step * x2 + sqrt(TminusU * UminusS / step) * pnl_rng_normal(rng);
+        i++;
+    }
+    // std::cout << x1 << " i " << i << "\n";
+    // return i * step / nbStep;
+    return step;
+}
+
+double monteCarloMeanPrecise(int nbSamples, double a, double T, double N, double steps)
+{
+    // instanciate the variables
+    double res = 0;
+
+    // Instanciate the rng
+    PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
+    pnl_rng_sseed(rng, time(NULL));
+
+    // loop for each sample
+    for (int i = 0; i <= nbSamples; i++)
+    {
+        // Get a brownian motion
+        PnlVect *bm = getBrownianMotion(N, T, rng);
+
+        // find ta and add it to res
+        double j = 1;
+        while (j <= N)
+        {
+            if (a > 0 && GET(bm, j) >= a)
+            {
+                res += getPreciseTA(a, GET(bm, j - 1), GET(bm, j), T / N, steps, rng) + (j - 1) * T / N;
+                j = N + 2;
+            }
+            else if (a < 0 && GET(bm, j) <= a)
+            {
+                res += getPreciseTA(a, GET(bm, j - 1), GET(bm, j), T / N, steps, rng) + (j - 1) * T / N;
+                j = N + 2;
+            }
+            else
+            {
+                j++;
+            };
+        }
+        if (j == N + 1)
+        {
+            res += T;
+        }
+
+        pnl_vect_free(&bm);
     }
 
     return res / nbSamples;
@@ -87,15 +146,19 @@ int main(int argc, char const *argv[])
     double a = 2;
     int nbSamples = 5000;
     double nbSteps = 5000;
+    double preciseSteps = 100;
     double practicalMean = 0;
+    double preciseMean = 0;
     double theoricalMean = 0;
 
     // Loop for each T value, compute and print the results
     for (double T = 1; T <= 5; T++)
     {
         practicalMean = monteCarloMean(nbSamples, 2, T, nbSteps);
+        preciseMean = monteCarloMeanPrecise(nbSamples, a, T, nbSteps, preciseSteps);
         theoricalMean = mean(2, T);
-        std::cout << "T = " << T << "\tPractical mean: " << practicalMean << "\t\tTheorical mean: " << theoricalMean << "\t\tbiais * sqrt(N): " << abs(practicalMean - theoricalMean) * sqrt(nbSteps) << "\n";
+        std::cout.precision(5);
+        std::cout << "T = " << T << "\tPractical mean: " << practicalMean << "\t\tPrecise mean: " << preciseMean << "\t\tTheorical mean: " << theoricalMean << "\t\tbiais * sqrt(N): " << abs(practicalMean - theoricalMean) * sqrt(nbSteps) << "\n";
     }
 
     return 0;
